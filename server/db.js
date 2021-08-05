10747cd7a26ab8ee2c5f9e6b2cfc3e40a6cc8670
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-08-05 11:33:20 +0800

    First commit

---
+++ server/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  database: process.env.DATABASE
+});
+
+module.exports = pool;
